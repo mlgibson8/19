12c2d5a4ca82ef7d3bec8073fa7bb248ea28ae42
mlgibson8 <michael.l.gibson@du.edu>
2023-07-05 10:24:50 -0600

    webpack testing

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,6 +4,30 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+plugins: [
+  new InjectManifest({
+    swSrc: './src-sw.js',
+    swDest: 'service-worker.js',
+    include: [/\.html$/, /\.js$/, /\.css$/, /\.png$/, /\.jpg$/, /\.jpeg$/, /\.svg$/],
+  }),
+  new WebpackPwaManifest({
+    name: 'Jate',
+    short_name: 'Jate',
+    description: 'Jate is a simple text eidtor',
+    //maybe change these for visibilty
+    theme_color: '#b1624d',
+    background_color: '#7b4d57',
+    //patch to icons
+    icons: [
+      {
+        src: path.resolve('src/assets/icon.png'),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join('assets', 'icons'),
+      },
+    ],
+  }),
+],
+
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -23,8 +47,23 @@ module.exports = () => {
 
     module: {
       rules: [
-        
-      ],
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets:['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread','@babel/transform-runtime'],
+        },
+      },
+    },
+    ],
     },
   };
 };
+
